a1610edebdebc48f597c3d9d023dadb06327ed4c
Marek Paterczyk <paterczm@users.noreply.github.com>
2025-05-21 08:51:19 -0400

    Track Turbonomic resource recommendations using cluster metrics (#4)

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -7,14 +7,47 @@ import (
 
 // Define custom metrics as package-level variables
 var (
+	dimensions = []string{"workload_namespace", "workload_kind", "workload_name"}
+
+	dimensions_with_container = append(dimensions, "workload_container")
+
 	TurboOverridesTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "turbonomic_companion_operator_turbo_override_total",
 		Help: "Total number of compute resource overrides performed by the webhook",
-	},
-		[]string{"workload_namespace", "workload_kind", "workload_name"})
+	}, dimensions)
+
+	TurboRecommendedTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "turbonomic_companion_operator_turbo_recommended_total",
+		Help: "Total number actions executed by Turbonomic",
+	}, dimensions)
+
+	TurboRecommendedRequestCpuGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "turbonomic_companion_operator_turbo_recommended_request_cpu",
+		Help: "Cpu requests recommended by Turbonomic",
+	}, dimensions_with_container)
+
+	TurboRecommendedRequestMemoryGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "turbonomic_companion_operator_turbo_recommended_request_memory",
+		Help: "Memory requests recommended by Turbonomic",
+	}, dimensions_with_container)
+
+	TurboRecommendedLimitCpuGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "turbonomic_companion_operator_turbo_recommended_limit_cpu",
+		Help: "Cpu limits recommended by Turbonomic",
+	}, dimensions_with_container)
+
+	TurboRecommendedLimitMemoryGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "turbonomic_companion_operator_turbo_recommended_limit_memory",
+		Help: "Memory limits recommended by Turbonomic",
+	}, dimensions_with_container)
 )
 
 // RegisterMetrics registers custom metrics with the global Prometheus registry
 func RegisterMetrics() {
 	ctrmetrics.Registry.MustRegister(TurboOverridesTotal)
+	ctrmetrics.Registry.MustRegister(TurboRecommendedTotal)
+	ctrmetrics.Registry.MustRegister(TurboRecommendedRequestCpuGauge)
+	ctrmetrics.Registry.MustRegister(TurboRecommendedRequestMemoryGauge)
+	ctrmetrics.Registry.MustRegister(TurboRecommendedLimitCpuGauge)
+	ctrmetrics.Registry.MustRegister(TurboRecommendedLimitMemoryGauge)
 }
